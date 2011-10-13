513fdec6ca3ea2c26b89ca97d6edc499e1205e3b
aayuso <aayuso@localhost>
2011-10-13 18:49:44 +0200

    New methods for Jongo Admin and to abstract different connections.

--- src/main/java/org/jongo/jdbc/JongoJDBCConnection.java
+++ src/main/java/org/jongo/jdbc/JongoJDBCConnection.java
@@ -1,5 +1,7 @@
 package org.jongo.jdbc;
 
+import java.util.Map;
+
 /**
  *
  * @author Alejandro Ayuso <alejandroayuso@gmail.com>
@@ -7,5 +9,23 @@ package org.jongo.jdbc;
 public interface JongoJDBCConnection {
     
     public void loadDriver();
+    
+    public String getInsertQuery(final String table, final Map<String,String> params);
+    
+    public String getUpdateQuery(final String table, final String key, final Map<String,String> params);
+    
+    public String getDeleteQuery(final String table, final String key);
+    
+    public String getCreateJongoTableQuery();
+    
+    public String getCreateJongoTableSequence();
+    
+    public String getCreateJongoTableTrigger();
+    
+    public String getCreateJongoQueryTable();
+    
+    public String getCreateJongoQuerySequence();
+    
+    public String getCreateJongoQueryTrigger();
     
 }
