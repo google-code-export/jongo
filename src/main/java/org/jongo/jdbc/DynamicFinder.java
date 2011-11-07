76131bf119775ae91baf7eb6ee0c6e93ea278e40
aayuso <aayuso@localhost>
2011-11-07 12:40:20 +0100

    Fixed the dynamic finder to work without dots.

--- src/main/java/org/jongo/jdbc/DynamicFinder.java
+++ src/main/java/org/jongo/jdbc/DynamicFinder.java
@@ -2,6 +2,8 @@ package org.jongo.jdbc;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.apache.commons.lang.StringUtils;
+import org.jongo.JongoUtils;
 import org.jongo.enums.Operator;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -11,12 +13,10 @@ import org.slf4j.LoggerFactory;
  * @author Alejandro Ayuso <alejandroayuso@gmail.com>
  */
 public class DynamicFinder {
-    
+
     private static final Logger l = LoggerFactory.getLogger(DynamicFinder.class);
-    
     public static final String FINDBY = "findBy";
     public static final String FINDALLBY = "findAllBy";
-    
     private String table;
     private String command;
     private String firstColumn;
@@ -25,68 +25,71 @@ public class DynamicFinder {
     private String secondColumn;
     private Operator secondOperator;
     private final String sql;
-    
-    public static DynamicFinder valueOf(String table, final String query, final String... values){
-        l.debug("Generating dynamic finder for " + query + " with values " + values);
-        final String [] splitted = validateAndSplitQuery(query);
-        final String command = parseCommand(splitted[0]);
-        
-        final List<Operator> operators = new ArrayList<Operator>();
-        final List<String> columns = new ArrayList<String>();
-        
-        for(int i = 1; i < splitted.length ; i++){
-            try{
-                operators.add(Operator.valueOf(splitted[i]));
-            }catch(IllegalArgumentException e){
-                columns.add(splitted[i]);
+
+    public static DynamicFinder valueOf(String table, final String query, final String... values) {
+        l.debug("Generating dynamic finder for " + query + " with values: [ " + StringUtils.join(values, ",") + "]");
+        String str = query;
+        String cmd = null;
+        if (str.contains(FINDBY)) {
+            str = str.substring(FINDBY.length());
+            cmd = FINDBY;
+        } else if (str.contains(FINDALLBY)) {
+            str = str.substring(FINDALLBY.length());
+            cmd = FINDALLBY;
+        } else {
+            throw new IllegalArgumentException("Invalid Command " + str);
+        }
+
+        String[] strs = JongoUtils.splitCamelCase(str).split("\\ ");
+        List<String> columns = new ArrayList<String>();
+        List<String> ops = new ArrayList<String>();
+        for (String word : strs) {
+            if (!Operator.keywords().contains(word)) {
+                columns.add(word);
+            } else {
+                ops.add(word);
             }
         }
+        List<Operator> operators = new ArrayList<Operator>();
+        String tmp = "";
+        for (int i = 0; i < ops.size(); i++) {
+            tmp += ops.get(i);
+            try {
+                Operator op = Operator.valueOf(tmp.toUpperCase());
+                if ((op == Operator.GREATERTHAN || op == Operator.LESSTHAN) && ((i + 1) < ops.size())) {
+                    Operator ope = Operator.valueOf(ops.get(i + 1).toUpperCase());
+                    if (ope == Operator.EQUALS) {
+                        op = Operator.valueOf(tmp.toUpperCase() + "EQUALS");
+                        ops.remove(i + 1);
+                    }
+                }
+                operators.add(op);
+                tmp = "";
+            } catch (IllegalArgumentException e) {}
+        }
         
-        if(operators.isEmpty()){
-            return new DynamicFinder(table, command, columns.get(0));
-        }else{
-            if(columns.size() == 1){
-                return new DynamicFinder(table, command, columns.get(0), operators.get(0));
-            }else if(columns.size() == 2){
-                if(operators.size() == 1){
-                    return new DynamicFinder(table, command, columns.get(0), operators.get(0), columns.get(1));
-                }else if(operators.size() == 2){
-                    return new DynamicFinder(table, command, columns.get(0), operators.get(0), columns.get(1), operators.get(1));
-                }else if(operators.size() == 3){
-                    return new DynamicFinder(table, command, columns.get(0), operators.get(0), operators.get(1), columns.get(1), operators.get(2));
-                }else{
+        if (operators.isEmpty()) {
+            return new DynamicFinder(table, cmd, columns.get(0));
+        } else {
+            if (columns.size() == 1) {
+                return new DynamicFinder(table, cmd, columns.get(0), operators.get(0));
+            } else if (columns.size() == 2) {
+                if (operators.size() == 1) {
+                    return new DynamicFinder(table, cmd, columns.get(0), operators.get(0), columns.get(1));
+                } else if (operators.size() == 2) {
+                    return new DynamicFinder(table, cmd, columns.get(0), operators.get(0), columns.get(1), operators.get(1));
+                } else if (operators.size() == 3) {
+                    return new DynamicFinder(table, cmd, columns.get(0), operators.get(0), operators.get(1), columns.get(1), operators.get(2));
+                } else {
                     throw new IllegalArgumentException("Too many operators: " + operators.size());
                 }
-                
-            }else{
+
+            } else {
                 throw new IllegalArgumentException("Too many columns: " + columns.size());
             }
         }
     }
     
-    private static String [] validateAndSplitQuery(final String query){
-        if(query == null){
-            throw new IllegalArgumentException("Empty query");
-        }
-        
-        String [] splitted = query.toUpperCase().split("\\.");
-        if(splitted.length < 2 || splitted.length > 6){
-            throw new IllegalArgumentException("Invalid query length " + splitted.length);
-        }
-        
-        return splitted;
-    }
-    
-    private static String parseCommand(final String str){
-        if(str.equalsIgnoreCase(FINDBY)){
-            return FINDBY;
-        }else if(str.equalsIgnoreCase(FINDALLBY)){
-            return FINDALLBY;
-        }else{
-            throw new IllegalArgumentException("Invalid Command " + str);
-        }
-    }
-
     /**
      * Creates a dynamic finder for findByName queries.
      * @param command either findBy or findAllBy
@@ -111,11 +114,11 @@ public class DynamicFinder {
         this.command = command;
         this.firstColumn = firstColumn;
         this.firstOperator = firstOperator;
-        if(this.firstOperator.isUnary()){
+        if (this.firstOperator.isUnary()) {
             this.sql = generateNullColumnQuery(this.table, this.firstColumn, this.firstOperator);
-        }else if (this.firstOperator == Operator.BETWEEN){
+        } else if (this.firstOperator == Operator.BETWEEN) {
             this.sql = generateBetweenQuery(this.table, this.firstColumn);
-        }else{
+        } else {
             this.sql = generateOneColumnQuery(this.table, this.firstColumn, this.firstOperator);
         }
     }
@@ -128,7 +131,9 @@ public class DynamicFinder {
      * @param secondColumn  command either findBy or findAllBy
      */
     public DynamicFinder(String table, String command, String firstColumn, Operator booleanOperator, String secondColumn) {
-        if(!booleanOperator.isBoolean()) throw new IllegalArgumentException("Invalid Operator " + booleanOperator);
+        if (!booleanOperator.isBoolean()) {
+            throw new IllegalArgumentException("Invalid Operator " + booleanOperator);
+        }
         this.table = table;
         this.command = command;
         this.firstColumn = firstColumn;
@@ -138,7 +143,7 @@ public class DynamicFinder {
         this.secondOperator = Operator.EQUALS;
         this.sql = generateTwoColumnQuery(this.table, this.firstColumn, this.firstOperator, this.booleanOperator, this.secondColumn, this.secondOperator);
     }
-    
+
     /**
      * Creates a dynamic finder for findByNameAndAgeGreaterThan
      * @param command command either findBy or findAllBy
@@ -148,7 +153,9 @@ public class DynamicFinder {
      * @param secondOperator a binary operator for the second column
      */
     public DynamicFinder(String table, String command, String firstColumn, Operator booleanOperator, String secondColumn, Operator secondOperator) {
-        if(!booleanOperator.isBoolean()) throw new IllegalArgumentException("Invalid Operator");
+        if (!booleanOperator.isBoolean()) {
+            throw new IllegalArgumentException("Invalid Operator");
+        }
         this.table = table;
         this.command = command;
         this.firstColumn = firstColumn;
@@ -156,9 +163,13 @@ public class DynamicFinder {
         this.booleanOperator = booleanOperator;
         this.secondColumn = secondColumn;
         this.secondOperator = secondOperator;
-        this.sql = generateTwoColumnQuery(this.table, this.firstColumn, this.firstOperator, this.booleanOperator, this.secondColumn, this.secondOperator);
+        if (this.firstOperator == Operator.BETWEEN) {
+            this.sql = generateBetweenQuery(this.table, this.firstColumn, this.booleanOperator, this.secondColumn, this.secondOperator);
+        }else{
+            this.sql = generateTwoColumnQuery(this.table, this.firstColumn, this.firstOperator, this.booleanOperator, this.secondColumn, this.secondOperator);
+        }
     }
-    
+
     /**
      * Creates a dynamic finder for findByNameNotEqualsAndAgeGreaterThan
      * @param command command either findBy or findAllBy
@@ -169,7 +180,9 @@ public class DynamicFinder {
      * @param secondOperator a binary operator for the second column
      */
     public DynamicFinder(String table, String command, String firstColumn, Operator firstOperator, Operator booleanOperator, String secondColumn, Operator secondOperator) {
-        if(!booleanOperator.isBoolean()) throw new IllegalArgumentException("Invalid Operator");
+        if (!booleanOperator.isBoolean()) {
+            throw new IllegalArgumentException("Invalid Operator");
+        }
         this.table = table;
         this.command = command;
         this.firstColumn = firstColumn;
@@ -177,10 +190,14 @@ public class DynamicFinder {
         this.booleanOperator = booleanOperator;
         this.secondColumn = secondColumn;
         this.secondOperator = secondOperator;
-        this.sql = generateTwoColumnQuery(this.table, this.firstColumn, this.firstOperator, this.booleanOperator, this.secondColumn, this.secondOperator);
+        if (this.firstOperator == Operator.BETWEEN) {
+            this.sql = generateBetweenQuery(this.table, this.firstColumn, this.booleanOperator, this.secondColumn, this.secondOperator);
+        }else{
+            this.sql = generateTwoColumnQuery(this.table, this.firstColumn, this.firstOperator, this.booleanOperator, this.secondColumn, this.secondOperator);
+        }
     }
 
-    private static String generateNullColumnQuery(String table, String firstColumn, Operator firstOperator){
+    private static String generateNullColumnQuery(String table, String firstColumn, Operator firstOperator) {
         StringBuilder sb = new StringBuilder("SELECT * FROM ");
         sb.append(table);
         sb.append(" WHERE ");
@@ -189,43 +206,44 @@ public class DynamicFinder {
         sb.append(firstOperator.sql());
         return sb.toString();
     }
-    
-    private static String generateOneColumnQuery(String table, String firstColumn, Operator firstOperator){
+
+    private static String generateOneColumnQuery(String table, String firstColumn, Operator firstOperator) {
         StringBuilder sb = new StringBuilder("SELECT * FROM ");
         sb.append(table);
         sb.append(" WHERE ");
         sb.append(firstColumn.toLowerCase());
         sb.append(" ");
         sb.append(firstOperator.sql());
-        if(!firstOperator.isUnary()){
+        if (!firstOperator.isUnary()) {
             sb.append(" ?");
         }
         return sb.toString();
     }
-    
-    private static String generateTwoColumnQuery(String table, String firstColumn, Operator firstOperator, Operator booleanOperator, String secondColumn, Operator secondOperator){
+
+    private static String generateTwoColumnQuery(String table, String firstColumn, Operator firstOperator, Operator booleanOperator, String secondColumn, Operator secondOperator) {
         StringBuilder sb = new StringBuilder("SELECT * FROM ");
         sb.append(table);
         sb.append(" WHERE ");
         sb.append(firstColumn);
         sb.append(" ");
         sb.append(firstOperator.sql());
-        if(!firstOperator.isUnary()){
+        if (!firstOperator.isUnary()) {
             sb.append(" ? ");
+        }else{
+            sb.append(" ");
         }
-        sb.append(" ");
         sb.append(booleanOperator.sql());
         sb.append(" ");
         sb.append(secondColumn);
         sb.append(" ");
         sb.append(secondOperator.sql());
-        if(!secondOperator.isUnary()){
-            sb.append(" ? ");
+        if (!secondOperator.isUnary()) {
+            sb.append(" ?");
         }
         return sb.toString();
     }
-    
-    private static String generateBetweenQuery(String table, String firstColumn){
+
+    private static String generateBetweenQuery(String table, String firstColumn) {
         StringBuilder sb = new StringBuilder("SELECT * FROM ");
         sb.append(table);
         sb.append(" WHERE ");
@@ -234,10 +252,27 @@ public class DynamicFinder {
         return sb.toString();
     }
     
+    private static String generateBetweenQuery(String table, String firstColumn, Operator booleanOperator, String secondColumn, Operator secondOperator) {
+        StringBuilder sb = new StringBuilder("SELECT * FROM ");
+        sb.append(table);
+        sb.append(" WHERE ");
+        sb.append(firstColumn.toLowerCase());
+        sb.append(" BETWEEN ? AND ? ");
+        sb.append(booleanOperator.sql());
+        sb.append(" ");
+        sb.append(secondColumn);
+        sb.append(" ");
+        sb.append(secondOperator.sql());
+        if (!secondOperator.isUnary()) {
+            sb.append(" ?");
+        }
+        return sb.toString();
+    }
+
     public String getCommand() {
         return command;
     }
-    
+
     public String getSql() {
         return sql;
     }
@@ -245,13 +280,13 @@ public class DynamicFinder {
     public String getTable() {
         return table;
     }
-    
-    public boolean findAll(){
+
+    public boolean findAll() {
         return this.command.equalsIgnoreCase(FINDALLBY);
     }
-    
+
     @Override
-    public String toString(){
+    public String toString() {
         StringBuilder b = new StringBuilder("DynamicFinder ");
         b.append("{ table : ");
         b.append(table);
@@ -275,5 +310,4 @@ public class DynamicFinder {
         b.append(" }");
         return b.toString();
     }
-    
 }
