a3d8392246932e57d24bb7f524600756c7062fb3
aayuso <aayuso@localhost>
2011-11-24 15:42:59 +0100

    New method to get all registries. Use JSON data instead query parameters for POST and PUT requests.

--- src/main/java/org/jongo/rest/JongoWS.java
+++ src/main/java/org/jongo/rest/JongoWS.java
@@ -1,9 +1,7 @@
 package org.jongo.rest;
 
 import java.util.List;
-import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 /**
  * Public RESTful webservice which allows CRUD operations on a given resource.
@@ -11,11 +9,12 @@ import javax.ws.rs.core.UriInfo;
  */
 public interface JongoWS {
     
+    public Response get(final String table, final String format);
     public Response get(final String table, final String format, final String id);
     public Response find(final String table, final String format, final String col, final String val);
     public Response findBy(final String table, final String format, final String query, final String value, final List<String> values);
-    public Response insert(final String table, final String format, final MultivaluedMap<String, String> formParams);
-    public Response update(final String table, final String format, final String id, final UriInfo ui);
+    public Response insert(final String table, final String format, final String jsonRequest);
+    public Response update(final String table, final String format, final String id, final String jsonRequest);
     public Response delete(final String table, final String format, final String id);
     public Response query(final String query, final String format, final List<String> arguments);
     
