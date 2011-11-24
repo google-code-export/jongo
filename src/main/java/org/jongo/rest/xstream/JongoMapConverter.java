c089eafd5b60bed60cc1b2c523100b9702fb4f88
aayuso <aayuso@localhost>
2011-11-24 13:08:55 +0100

    Unmarshal a json request to a map (not tested with XML)

--- src/main/java/org/jongo/rest/xstream/JongoMapConverter.java
+++ src/main/java/org/jongo/rest/xstream/JongoMapConverter.java
@@ -1,5 +1,6 @@
 package org.jongo.rest.xstream;
 
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import com.thoughtworks.xstream.converters.Converter;
 import com.thoughtworks.xstream.converters.MarshallingContext;
 import com.thoughtworks.xstream.converters.UnmarshallingContext;
@@ -7,6 +8,7 @@ import com.thoughtworks.xstream.io.HierarchicalStreamReader;
 import com.thoughtworks.xstream.io.HierarchicalStreamWriter;
 import java.util.HashMap;
 import java.util.Map;
+import javax.ws.rs.core.MultivaluedMap;
 
 /**
  *
@@ -32,12 +34,26 @@ public class JongoMapConverter implements Converter {
 
     @Override
     public Object unmarshal(HierarchicalStreamReader reader, UnmarshallingContext uc) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        Map<String, Object> map = new HashMap<String, Object>();
+        MultivaluedMap<String, String> mv = new MultivaluedMapImpl();
+        while(reader.hasMoreChildren()){
+            reader.moveDown();
+            mv.add(reader.getNodeName(), reader.getValue());
+            map.put(reader.getNodeName(), reader.getValue());
+            reader.moveUp();
+        }
+        
+        if(uc.getRequiredType().equals(MultivaluedMap.class)){
+            return mv;
+        }else{
+            return map;
+        }
+        
     }
 
     @Override
     public boolean canConvert(Class type) {
-        return type.equals(HashMap.class);
+        return type.equals(HashMap.class) || type.equals(MultivaluedMap.class);
     }
     
 }
