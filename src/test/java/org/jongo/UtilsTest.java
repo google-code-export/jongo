9c8dd377efa8c00f311d5465815237f31f20d9e6
aayuso <aayuso@localhost>
2011-12-16 18:49:03 +0100

    [TAG] Work on (Issue 5) adding limit and order parameters

--- src/test/java/org/jongo/UtilsTest.java
+++ src/test/java/org/jongo/UtilsTest.java
@@ -17,11 +17,16 @@
  */
 package org.jongo;
 
+import com.sun.jersey.core.util.MultivaluedMapImpl;
 import java.math.BigDecimal;
 import java.text.ParseException;
+import javax.ws.rs.core.MultivaluedMap;
 import org.joda.time.DateTime;
 import org.joda.time.format.DateTimeFormatter;
 import org.joda.time.format.ISODateTimeFormat;
+import org.jongo.jdbc.LimitParam;
+import org.jongo.jdbc.OrderParam;
+import org.jongo.jdbc.connections.HSQLConnection;
 import static org.junit.Assert.*;
 
 import org.junit.Test;
@@ -66,10 +71,56 @@ public class UtilsTest {
     public void parseValue(){
         assertTrue(JongoUtils.parseValue("1") instanceof Integer);
         assertTrue(JongoUtils.parseValue("1.0") instanceof BigDecimal);
-        assertTrue(JongoUtils.parseValue("") instanceof String);
+        assertTrue(JongoUtils.parseValue(" ") instanceof String);
         assertTrue(JongoUtils.parseValue("false") instanceof String);
         assertTrue(JongoUtils.parseValue("true") instanceof String);
         assertTrue(JongoUtils.parseValue("2011-12-11T12:35:45.200+01:00") instanceof java.sql.Date);
         assertTrue(JongoUtils.parseValue("2011-01-19") instanceof java.sql.Date);
+    }
+    
+    @Test(expected=IllegalArgumentException.class)
+    public void testOrderParam(){
+        MultivaluedMap<String, String> formParams = new MultivaluedMapImpl();
+        assertEquals(OrderParam.valueOf(formParams).toString(), "{OrderParam:{column:\"id\", direction:\"ASC\"}}");
+        formParams.add("dir", "KKK");
+        OrderParam.valueOf(formParams).toString(); // throw Exception!
+        formParams.add("dir", "DESC");
+        assertEquals(OrderParam.valueOf(formParams).toString(), "{OrderParam:{column:\"id\", direction:\"ASC\"}}");
+        formParams.add("sort", "kkk");
+        assertEquals(OrderParam.valueOf(formParams).toString(), "{OrderParam:{column:\"kkk\", direction:\"DESC\"}}");
+        formParams.remove("dir");
+        assertEquals(OrderParam.valueOf(formParams).toString(), "{OrderParam:{column:\"kkk\", direction:\"ASC\"}}");
+    }
+    
+    @Test
+    public void testLimitParam(){
+        MultivaluedMap<String, String> formParams = new MultivaluedMapImpl();
+        LimitParam instance = LimitParam.valueOf(formParams);
+        assertEquals(instance.getLimit(), new Integer(25));
+        assertEquals(instance.getStart(), new Integer(0));
+        formParams.putSingle("offset", "test");
+        instance = LimitParam.valueOf(formParams);
+        assertEquals(instance.getLimit(), new Integer(25));
+        assertEquals(instance.getStart(), new Integer(0));
+        formParams.putSingle("offset", "50");
+        instance = LimitParam.valueOf(formParams);
+        assertEquals(instance.getLimit(), new Integer(25));
+        assertEquals(instance.getStart(), new Integer(0));
+        formParams.putSingle("limit", "100");
+        instance = LimitParam.valueOf(formParams);
+        assertEquals(instance.getLimit(), new Integer(100));
+        assertEquals(instance.getStart(), new Integer(50));
+        formParams.remove("offset");
+        instance = LimitParam.valueOf(formParams);
+        assertEquals(instance.getLimit(), new Integer(100));
+        assertEquals(instance.getStart(), new Integer(0));
+    }
+    
+    @Test
+    public void testSQL() throws Exception{
+        LimitParam l = new LimitParam();
+        OrderParam o = new OrderParam();
+        HSQLConnection c = new HSQLConnection("jdbc", "k", "k");
+        System.out.println(c.getSelectAllFromTableQuery("table", l, o));
     }
 }
