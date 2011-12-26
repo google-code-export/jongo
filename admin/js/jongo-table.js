21e222c23409c0ae06f86f2ea769b5e24614e612
ubersoldat <ubersoldat@uberws>
2011-12-26 15:34:15 +0100

    Fixed to use a JSON request. Added license.

--- admin/js/jongo-table.js
+++ admin/js/jongo-table.js
@@ -1,8 +1,26 @@
+/**
+ * Copyright (C) 2011, 2012 Alejandro Ayuso
+ *
+ * This file is part of Jongo.
+ * Jongo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * Jongo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Jongo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 function getJongoTables(){
     debug('Obtaining Jongo Tables');
     var tables = new Array();
     $.ajax({
-        url: '/adminws/table/all',
+        url: '/adminws/table',
         async: false,
         dataType: 'json',
         success: function(data){
@@ -110,17 +128,18 @@ function addTableDialog(componentName){
 }
 
 function editTable(id){
-    var data = new Array(3);
+    var data = {}
     var tableName = $("#tableName" + id).val()
-    data.push("name=" + tableName);
-    data.push("customId=" + $("#tableCustomId" + id).val());
-    data.push("permits=" + getPermissionValue(id));
+    data['name'] = tableName;
+    data['customId'] = $("#tableCustomId" + id).val();
+    data['permits'] = getPermissionValue(id)
     
     var ret = $.ajax({
         type: 'PUT',
-        url: '/adminws/table/' + id + '?' + data.join('&'),
+        contentType: "application/json",
+        url: '/adminws/table/' + id,
         success: function() {},
-        data: data,
+        data: JSON.stringify(data),
         dataType: 'json'
     });
     
