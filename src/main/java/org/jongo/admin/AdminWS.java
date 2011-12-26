62eb547e164120e95f1901706212c4b9099b4b7a
ubersoldat <ubersoldat@uberws>
2011-12-26 13:21:56 +0100

    Don't use path params. Instead use json params

--- src/main/java/org/jongo/admin/AdminWS.java
+++ src/main/java/org/jongo/admin/AdminWS.java
@@ -21,7 +21,6 @@ package org.jongo.admin;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.core.MultivaluedMap;
 import javax.ws.rs.core.Response;
-import javax.ws.rs.core.UriInfo;
 
 /**
  *
@@ -31,12 +30,12 @@ public interface AdminWS {
     public Response getJongoTable(final HttpServletRequest request, final String format);
     public Response getJongoTable(final String resourceId, final HttpServletRequest request, final String format);
     public Response insertJongoTable(final HttpServletRequest request, final String format, final MultivaluedMap<String, String> formParams);
-    public Response updateJongoTable(final HttpServletRequest request, final String format, final String id, final UriInfo ui);
+    public Response updateJongoTable(final HttpServletRequest request, final String format, final String id, final String jsonRequest);
     public Response deleteJongoTable(final HttpServletRequest request, final String format, final String id);
     public Response getJongoQuery(final HttpServletRequest request, final String format);
     public Response getJongoQuery(final String queryId, final HttpServletRequest request, final String format);
     public Response insertJongoQuery(final HttpServletRequest request, final String format, final MultivaluedMap<String, String> formParams);
-    public Response updateJongoQuery(final HttpServletRequest request, final String format, final String id, final UriInfo ui);
+    public Response updateJongoQuery(final HttpServletRequest request, final String format, final String id, final String jsonRequest);
     public Response deleteJongoQuery(final HttpServletRequest request, final String format, final String id);
     public Response getJongoStatistics(final String format);
 }
