fdb5b658e01b2ac053f30e26a413e38fcb285ec7
ubersoldat <ubersoldat@uberws>
2012-01-09 15:34:50 +0100

    Fixed the regex to correctly handle columns with _ and 0-9 names

--- src/main/java/org/jongo/JongoUtils.java
+++ src/main/java/org/jongo/JongoUtils.java
@@ -98,7 +98,7 @@ public class JongoUtils {
      * @return same string with white spaces.
      */
     public static String splitCamelCase(String s) {
-        return s.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z])"), " ");
+        return s.replaceAll(String.format("%s|%s|%s", "(?<=[A-Z])(?=[A-Z][a-z])", "(?<=[^A-Z])(?=[A-Z])", "(?<=[A-Za-z])(?=[^A-Za-z0-9_])"), " ");
     }
     
     /**
