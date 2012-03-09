05e169006e6f9b1b578114f4158353a13c6de2ef
ubersoldat <ubersoldat@uberws>
2012-03-09 16:47:00 +0100

    Last commit with the correct error code values from HSQLD.

--- src/main/java/org/jongo/jdbc/exceptions/HSQLException.java
+++ src/main/java/org/jongo/jdbc/exceptions/HSQLException.java
@@ -18,105 +18,55 @@
 
 package org.jongo.jdbc.exceptions;
 
-import java.sql.SQLException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import org.jongo.JongoUtils;
 
 /**
- * Re-map the HSQLDB error codes from org.hsqldb.error.ErrorCode.java to
- * something we can handle.
+ * Re-map the HSQLDB error codes from org.hsqldb.error.ErrorCode.java to HTTP error codes.
  * @author Alejandro Ayuso <alejandroayuso@gmail.com>
  */
 public class HSQLException extends JongoJDBCException {
     
-    private static final Integer [] dataIntegrityViolationErrors = {8, 10, 104, 157, 177, 3500, 3501};
-
-    public HSQLException(String msg) {
-        super(msg);
-    }
+    private static final long serialVersionUID = 1L;
     
-    public HSQLException(String msg, SQLException e) {
-        super(msg,e);
-    }
+    private static final List<Integer> badRequestErrors = new ArrayList<Integer>();
+    private static final List<Integer> badGatewayErrors = new ArrayList<Integer>();
+    private static final List<Integer> forbiddenErrors = new ArrayList<Integer>();
+    private static final List<Integer> notFoundErrors = new ArrayList<Integer>();
     
-    public HSQLException(String msg, int e) {
-        super(msg,e);
-    }
-
-    /**
-     * Was db exception caused by bad sql grammer (a typo)
-     * return true or false
-     */
-    @Override
-    public boolean isBadSQLGrammar() {
-        return (((sqlErrorCode >= 67) && (sqlErrorCode <= 71)) || sqlErrorCode == 5 || sqlErrorCode == 11 || sqlErrorCode == 12
-                || sqlErrorCode == 13 || sqlErrorCode == 58 || sqlErrorCode == 74 || sqlErrorCode == 121 || sqlErrorCode == 5581
-                || (sqlErrorCode >= 3400 && sqlErrorCode < 3499) );
-    }
-
-    /**
-     * Was db exception caused by a data integrity violation
-     * @return true or false
-     */
-    @Override
-    public boolean isDataIntegrityViolation() {
-        
-        return (Arrays.binarySearch(dataIntegrityViolationErrors, sqlErrorCode) >= 0);
-    }
-
-    /**
-     * Was db exception caused by database being unavailable
-     * @return true or false
-     */
-    @Override
-    public boolean isDatabaseUnavailable() {
-        return (((sqlErrorCode >= 1) && (sqlErrorCode <= 4)) || sqlErrorCode == 94);
-    }
-
-    /**
-     * Was db exception caused by referencing a invalid bind parameter name
-     * @return true or false
-     */
-    @Override
-    public boolean isInvalidBindVariableName() {
-        return (sqlErrorCode == 216);
-    }
-
-    /**
-     * Was db exception caused by referencing a non existent table or view
-     * @return true or false
-     */
-    @Override
-    public boolean isNonExistentTableOrViewOrCol() {
-        return (sqlErrorCode == 22 || sqlErrorCode == 28 || sqlErrorCode == 53 || sqlErrorCode == 3603 || sqlErrorCode == 5501);
-    }
-
-    /**
-     * Was db exception caused by a row lock error or some other sql query timeout
-     * return true or false
-     */
-    @Override
-    public boolean isRowlockOrTimedOut() {
-        return false;
-    }
-
-    /**
-     * Was db exception caused by a duplicate record (unique constraint) violation 
-     */
-    @Override
-    public boolean isUniqueConstraintViolation() {
-        return ((sqlErrorCode == 9) || (sqlErrorCode == 104));
-    }
-
-    /** was db exception caused by a an unbound variable (parameter)
-     * @return boolean
-     */
-    @Override
-    public boolean isVarParameterUnbound() {
-        return false;
+    static{
+        badRequestErrors.addAll(Arrays.asList(new Integer[]{8, 10, 104, 157, 177, 3500, 3501, 2100, 3201, 3800, 5502, 5504, 5513, 5, 11, 12, 13, 58, 74, 121, 5581, 81, 70, 71, 72, 5000, 5510, 5512, 216, 5546, 5505, 5506}));
+        badRequestErrors.addAll(JongoUtils.range(5520, 5539));
+        badGatewayErrors.addAll(JongoUtils.range(1300, 1306));
+        badGatewayErrors.addAll(Arrays.asList(new Integer[]{1,2,3,4,23,61,62,63,64,65,401,402,403,404,405,406,407}));
+        forbiddenErrors.addAll(Arrays.asList(new Integer[]{5501, 5503, 5507, 5508, 5509, 5545}));
+        notFoundErrors.addAll(Arrays.asList(new Integer[]{5505, 5506}));
     }
 
     @Override
-    public boolean isReadOnly() {
-        return sqlErrorCode == 456;
+    public boolean isBadGateway() {
+        return badGatewayErrors.contains(Math.abs(getErrorCode()));
+    }
+
+    @Override
+    public boolean isGatewayTimeout() {
+        return Math.abs(getErrorCode()) == 1351;
+    }
+
+    @Override
+    public boolean isBadRequest() {
+        return badRequestErrors.contains(Math.abs(getErrorCode()));
+    }
+
+    @Override
+    public boolean isForbidden() {
+        return forbiddenErrors.contains(Math.abs(getErrorCode()));
+    }
+
+    @Override
+    public boolean isNotFound() {
+        return notFoundErrors.contains(Math.abs(getErrorCode()));
     }
 }
