30e40fe6ff1bec308974cb540d9e976d219044b9
alejandro.ayuso <alejandro.ayuso@localhost>
2012-03-27 10:06:22 +0200

    Added a toXML method.

--- src/main/java/org/jongo/rest/xstream/RowResponse.java
+++ src/main/java/org/jongo/rest/xstream/RowResponse.java
@@ -1,21 +1,16 @@
 /**
  * Copyright (C) 2011, 2012 Alejandro Ayuso
  *
- * This file is part of Jongo.
- * Jongo is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * any later version.
- * 
- * Jongo is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- * 
- * You should have received a copy of the GNU General Public License
- * along with Jongo.  If not, see <http://www.gnu.org/licenses/>.
+ * This file is part of Jongo. Jongo is free software: you can redistribute it and/or modify it under the terms of the
+ * GNU General Public License as published by the Free Software Foundation, either version 3 of the License, or any
+ * later version.
+ *
+ * Jongo is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty
+ * of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with Jongo. If not, see
+ * <http://www.gnu.org/licenses/>.
  */
-
 package org.jongo.rest.xstream;
 
 import java.util.ArrayList;
@@ -29,46 +24,65 @@ import org.apache.commons.lang.StringUtils;
  * @author Alejandro Ayuso <alejandroayuso@gmail.com>
  */
 public class RowResponse {
+
     private final int roi;
-    private final Map<String, String> columns;// = new HashMap<String, String>();
+    private final Map<String, String> cells;// = new HashMap<String, String>();
 
     public RowResponse(int roi) {
         this.roi = roi;
-        this.columns = new HashMap<String, String>();
+        this.cells = new HashMap<String, String>();
     }
-    
+
     public RowResponse(int roi, Map<String, String> columns) {
         this.roi = roi;
-        this.columns = columns;
+        this.cells = columns;
     }
 
     public int getRoi() {
         return roi;
     }
 
-    public Map<String, String> getColumns() {
-        return columns;
+    public Map<String, String> getCells() {
+        return cells;
     }
-    
-    public String toJSON(){
+
+    public String toJSON() {
         List<String> args = new ArrayList<String>();
-        for(String key: columns.keySet()){
-            String val = columns.get(key);
-            if(StringUtils.isNumeric(val)){
-                if(StringUtils.isWhitespace(val)){
+        for (String key : cells.keySet()) {
+            String val = cells.get(key);
+            if (StringUtils.isNumeric(val)) {
+                if (StringUtils.isWhitespace(val)) {
                     args.add("\"" + key.toLowerCase() + "\"" + ":" + "\"\"");
-                }else{
+                } else {
                     args.add("\"" + key.toLowerCase() + "\"" + ":" + val);
                 }
-            }else{
+            } else {
                 args.add("\"" + key.toLowerCase() + "\"" + ":" + "\"" + val + "\"");
             }
         }
-        
+
         StringBuilder b = new StringBuilder();
         b.append("{");
         b.append(StringUtils.join(args, ","));
         b.append("}");
+        return b.toString();
+    }
+
+    public String toXML() {
+        StringBuilder b = new StringBuilder("<row roi=\"");
+        b.append(roi);
+        b.append("\"><cells>");
+        for (String key : cells.keySet()) {
+            String val = cells.get(key);
+            b.append("<");
+            b.append(key.toLowerCase());
+            b.append(">");
+            b.append(val);
+            b.append("</");
+            b.append(key.toLowerCase());
+            b.append(">");
+        }
+        b.append("</cells></row>");
         return b.toString();
     }
 }
