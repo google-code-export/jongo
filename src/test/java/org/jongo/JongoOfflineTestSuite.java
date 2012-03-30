1361c5f174eb75ed614a22d1ca75eac1d6c7862c
ubersoldat <ubersoldat@uberws>
2012-03-30 16:11:58 +0200

    Add the test for Oracle databases configuration.

--- src/test/java/org/jongo/JongoOfflineTestSuite.java
+++ src/test/java/org/jongo/JongoOfflineTestSuite.java
@@ -36,7 +36,7 @@ import org.junit.runners.Suite;
     org.jongo.config.AbstractDatabaseConfigurationTest.class,
     org.jongo.config.impl.HSQLDBConfigurationTest.class,
     org.jongo.config.impl.MySQLConfigurationTest.class,
-    org.jongo.config.impl.OracleConfigurationTest.class,
+    org.jongo.config.impl.OracleConfigurationTest.class
 })
 public class JongoOfflineTestSuite {
 
