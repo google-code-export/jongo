1f9cd183e4885deb6b2db6175ca34ce566cfaaeb
ubersoldat <ubersoldat@uberws>
2012-04-02 00:29:41 +0200

    Changed the name from idField to pk (primary key)

--- src/main/java/org/jongo/JongoWS.java
+++ src/main/java/org/jongo/JongoWS.java
@@ -29,16 +29,39 @@ import javax.ws.rs.core.UriInfo;
  */
 public interface JongoWS {
     
+    /**
+     * REST gateway for database metadata
+     * @param database name of the database we want to access
+     * @return depends on database implementations but probably, a list of the
+     * tables defined in the database or schema.
+     */
     public Response dbMeta(String database);
+    
+    /**
+     * REST gateway for resource metadata. A resource can be a table, a view, etc.
+     * @param database name of the database the resource belongs to
+     * @param resource name of the resource we want to access
+     * @return metadata about the resource, like its fields.
+     */
     public Response resourceMeta(String database, String resource);
-    public Response get(String database, String resource, String idField, String id, UriInfo ui);
+    
+    /**
+     * Obtains the requested resource
+     * @param database name of the database the resource belongs to
+     * @param resource name of the resource we want to access
+     * @param pk optional field which indicates the primary key column name
+     * @param id the primary key value of the resource we want to access
+     * @param ui the context of the request. Used to obtain the ordering and pagination parameters.
+     * @return the resource if it's found, or a 404 if it's not.
+     */
+    public Response get(String database, String resource, String pk, String id, UriInfo ui);
     public Response getAll(String database, String resource, UriInfo ui);
     public Response find(String database, String resource, String col, String arg, UriInfo ui);
     public Response findBy(String database, String resource, String query, List<String> args, UriInfo ui);
-    public Response insert(String database, String resource, String idField, String jsonRequest);
-    public Response insert(String database, String resource, String idField, MultivaluedMap<String, String> formParams);
-    public Response update(String database, String resource, String idField, String id, String jsonRequest);
-    public Response delete(String database, String resource, String idField, String id);
+    public Response insert(String database, String resource, String pk, String jsonRequest);
+    public Response insert(String database, String resource, String pk, MultivaluedMap<String, String> formParams);
+    public Response update(String database, String resource, String pk, String id, String jsonRequest);
+    public Response delete(String database, String resource, String pk, String id);
     public Response storedProcedure(String database, String query, String jsonRequest);
     public Response getJongoStatistics();
     
