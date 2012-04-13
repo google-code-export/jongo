ee713a79cc362bff4f3e5be9d873b7b2f342a7ca
ubersoldat <ubersoldat@uberws>
2012-04-13 18:42:53 +0200

    Deprecate QueryParams for the SQL params (Select, Update, etc)

--- src/main/java/org/jongo/jdbc/QueryParams.java
+++ src/main/java/org/jongo/jdbc/QueryParams.java
@@ -27,6 +27,7 @@ import org.apache.commons.lang.StringUtils;
  * Holder for a query parameters.
  * @author Alejandro Ayuso <alejandroayuso@gmail.com>
  */
+@Deprecated
 public class QueryParams {
     
     private String database;
