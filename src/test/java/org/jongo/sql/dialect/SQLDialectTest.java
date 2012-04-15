827d54f5de4006525a1afdf4b5f81655c36b64fc
ubersoldat <ubersoldat@uberlaptop>
2012-04-15 14:00:08 +0200

    Removed configuration initialization. Fixed the SQL strings.

--- src/test/java/org/jongo/sql/dialect/SQLDialectTest.java
+++ src/test/java/org/jongo/sql/dialect/SQLDialectTest.java
@@ -1,74 +1,57 @@
 package org.jongo.sql.dialect;
 
 import junit.framework.Assert;
-import org.jongo.JongoUtils;
-import org.jongo.config.JongoConfiguration;
-import org.jongo.demo.Demo;
-import org.jongo.exceptions.StartupException;
 import org.jongo.jdbc.LimitParam;
 import org.jongo.jdbc.OrderParam;
 import org.jongo.sql.*;
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
  *
- * @author Alejandro Ayuso <alejandroayuso@gmail.com>
+ * @author Alejandro Ayuso
  */
 public class SQLDialectTest {
     
     Table table = new Table("demo1", "a_table", "tableId");
-    Dialect d = new SQLDialect();
+    Dialect d;
     
     OrderParam o = new OrderParam(table);
     LimitParam l = new LimitParam();
     
-    @BeforeClass
-    public static void setUp() throws StartupException{
-        System.setProperty("environment", "demo");
-        JongoConfiguration configuration = JongoUtils.loadConfiguration();
-        Demo.generateDemoDatabases(configuration.getDatabases());
+    public SQLDialectTest(){
+        d = new SQLDialect();
     }
     
-    @AfterClass
-    public static void tearDownClass() throws Exception {
-        System.setProperty("environment", "demo");
-        JongoConfiguration configuration = JongoUtils.loadConfiguration();
-        Demo.destroyDemoDatabases(configuration.getDatabases());
-    }
-
     @Test
     public void testSelect() {
-        doTest("SELECT * FROM demo1.a_table", new Select(table));
+        doTest("SELECT * FROM a_table", new Select(table));
         
-        doTest("SELECT * FROM demo1.a_table WHERE a_table.tableId=?", new Select(table).setValue("1"));
+        doTest("SELECT * FROM a_table WHERE tableId=?", new Select(table).setValue("1"));
         
-        doTest("SELECT * FROM demo1.a_table WHERE a_table.name=?", new Select(table).setValue("1").setColumn("name"));
+        doTest("SELECT * FROM a_table WHERE name=?", new Select(table).setValue("1").setColumn("name"));
         
-        doTest("SELECT * FROM demo1.a_table WHERE a_table.name=? LIMIT 25 OFFSET 0",
+        doTest("SELECT * FROM a_table WHERE name=? LIMIT 25 OFFSET 0",
                 new Select(table).setValue("1").setColumn("name").setLimitParam(new LimitParam()));
         
-        doTest("SELECT * FROM demo1.a_table WHERE a_table.tableId=? ORDER BY a_table.tableId ASC LIMIT 25 OFFSET 0",
+        doTest("SELECT * FROM a_table WHERE tableId=? ORDER BY tableId ASC LIMIT 25 OFFSET 0",
                 new Select(table).setValue("1").setLimitParam(l).setOrderParam(new OrderParam(table)));
     }
     
     @Test
     public void testDelete(){
-        doTest("DELETE FROM demo1.a_table WHERE a_table.tableId=?", new Delete(table).setId("1"));
-        doTest("DELETE FROM demo1.grrr WHERE grrr.id=?", new Delete(new Table("demo1", "grrr")).setId("1"));
+        doTest("DELETE FROM a_table WHERE tableId=?", new Delete(table).setId("1"));
+        doTest("DELETE FROM grrr WHERE id=?", new Delete(new Table("demo1", "grrr")).setId("1"));
     }
     
     @Test
     public void testInsert(){
-        doTest("INSERT INTO demo1.a_table (name,age) VALUES (?,?)", new Insert(table).addColumn("name", "foo bar").addColumn("age", "50"));
-        doTest("INSERT INTO demo1.a_table", new Insert(table));
+        doTest("INSERT INTO a_table (name,age) VALUES (?,?)", new Insert(table).addColumn("name", "foo bar").addColumn("age", "50"));
     }
     
     @Test
     public void testUpdate(){
-        doTest("UPDATE demo1.a_table SET name=?,age=? WHERE a_table.tableId=?", new Update(table).setId("1").addColumn("name", "foo bar").addColumn("age", "50"));
-        doTest("UPDATE demo1.grrr SET name=? WHERE grrr.id=?", new Update(new Table("demo1","grrr")).setId("1").addColumn("name", "foo bar"));
+        doTest("UPDATE a_table SET name=?,age=? WHERE tableId=?", new Update(table).setId("1").addColumn("name", "foo bar").addColumn("age", "50"));
+        doTest("UPDATE grrr SET name=? WHERE id=?", new Update(new Table("demo1","grrr")).setId("1").addColumn("name", "foo bar"));
     }
     
     public void doTest(String expected, Object obj){
