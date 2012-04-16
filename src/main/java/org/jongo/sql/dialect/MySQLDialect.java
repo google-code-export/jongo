12fef901d9a5d0c6ace7e45839939f4967875cd6
ubersoldat <ubersoldat@uberws>
2012-04-16 15:31:40 +0200

    Translate sql select to mysql select query

--- src/main/java/org/jongo/sql/dialect/MySQLDialect.java
+++ src/main/java/org/jongo/sql/dialect/MySQLDialect.java
@@ -17,15 +17,52 @@
  */
 package org.jongo.sql.dialect;
 
+import org.apache.commons.lang.StringUtils;
+import org.jongo.sql.Select;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  *
  * @author Alejandro Ayuso <alejandroayuso@gmail.com>
  */
 public class MySQLDialect extends SQLDialect {
-
+    
+    private static final Logger l = LoggerFactory.getLogger(MySQLDialect.class);
+    
     @Override
     public String listOfTablesStatement() {
         return "SHOW TABLES";
+    }
+    
+    @Override
+    public String toStatementString(Select select) {
+        final StringBuilder b = new StringBuilder("SELECT ");
+        if(select.isAllColumns()){
+            b.append("t.*");
+        }else{
+            for(String col : select.getColumns())
+                b.append("t.").append(col).append(",");
+            b.deleteCharAt(b.length()-1);
+        }
+        b.append(" FROM ").append(select.getTable().getDatabase()).append(".");
+        b.append(select.getTable().getName()).append(" t");
+        if(!select.isAllRecords()){
+            b.append(" WHERE ");
+            if(StringUtils.isEmpty(select.getColumn())){
+                b.append("t.").append(select.getTable().getPrimaryKey()).append("=?");
+            }else{
+                b.append("t.").append(select.getColumn()).append("=?");
+            }
+        }
+        if(select.getOrderParam() != null)
+            b.append(" ORDER BY t.").append(select.getOrderParam().getColumn()).append(" ").append(select.getOrderParam().getDirection());
+        
+        if(select.getLimitParam() != null)
+            b.append(" LIMIT ").append(select.getLimitParam().getStart()).append(",").append(select.getLimitParam().getLimit());
+        
+        l.debug(b.toString());
+        return b.toString();
     }
     
 }
