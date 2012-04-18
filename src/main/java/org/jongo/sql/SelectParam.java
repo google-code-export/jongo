50d7c1bcd74138a8fb69b24b995c94f6e4776f74
ubersoldat <ubersoldat@uberws>
2012-04-18 19:01:27 +0200

    Documentation and new sql method to make it easier to append to builders

--- src/main/java/org/jongo/sql/SelectParam.java
+++ src/main/java/org/jongo/sql/SelectParam.java
@@ -1,15 +1,44 @@
+/**
+ * Copyright (C) 2011, 2012 Alejandro Ayuso
+ *
+ * This file is part of Jongo.
+ * Jongo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * any later version.
+ * 
+ * Jongo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * 
+ * You should have received a copy of the GNU General Public License
+ * along with Jongo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package org.jongo.sql;
 
 import org.jongo.enums.Operator;
 
 /**
- *
+ * Class for Select parameters.
  * @author Alejandro Ayuso
  */
 public class SelectParam {
     
+    /**
+     * Name of the column to use in the query.
+     */
     private String columnName;
+    
+    /**
+     * {@link org.jongo.enums.Operator} to use in the query.
+     */
     private Operator operator;
+    
+    /**
+     * Value the query should match.
+     */
     private String value;
 
     public SelectParam() {}
@@ -60,8 +89,7 @@ public class SelectParam {
         return this;
     }
 
-    @Override
-    public String toString() {
+    public String sql() {
         StringBuilder b = new StringBuilder(columnName).append(" ").append(operator.sql()).append(" ?");
         return b.toString();
     }
