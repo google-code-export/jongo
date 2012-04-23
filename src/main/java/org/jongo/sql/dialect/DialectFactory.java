833d5aaa776bfe2c5f2a8dfbfbc3886d1fd92316
ubersoldat <ubersoldat@uberws>
2012-04-23 19:03:47 +0200

    Provide dialect for new drivers

--- src/main/java/org/jongo/sql/dialect/DialectFactory.java
+++ src/main/java/org/jongo/sql/dialect/DialectFactory.java
@@ -3,19 +3,32 @@ package org.jongo.sql.dialect;
 import org.jongo.enums.JDBCDriver;
 
 /**
- * 
+ * Factory method to generate SQL Dialects
  * @author Alejandro Ayuso 
  */
 public class DialectFactory {
     
+    /**
+     * For a given driver, return the appropriate dialect.
+     * @param driver a {@link org.jongo.enums.JDBCDriver}
+     * @return a {@link org.jongo.sql.dialect.Dialect} for the driver.
+     */
     public static Dialect getDialect(final JDBCDriver driver){
         Dialect dialect; 
         switch(driver){
-            case HSQLDB: dialect = new HSQLDialect(); break;
+            case HSQLDB_MEM:
+            case HSQLDB_FILE:
+                dialect = new HSQLDialect(); 
+                break;
+            
+            case MSSQL_JTDS:
+            case MSSQL: 
+                dialect = new MSSQLDialect(); 
+                break;
+                
             case MySQL: dialect = new MySQLDialect(); break;
             case ORACLE: dialect = new OracleDialect(); break;
             case PostgreSQL: dialect = new PostgreSQLDialect(); break;
-            case MSSQL: dialect = new MSSQLDialect(); break;
             default: dialect = new SQLDialect(); break;
         }
         return dialect;
