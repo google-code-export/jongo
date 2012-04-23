f5f62bdfa812ce5d5e5b4877686e876e0d49a6fb
ubersoldat <ubersoldat@uberws>
2012-04-23 18:22:40 +0200

    Remove the JDBC URL field and use the database, host and port fields. Allows for more flexibility.

--- src/main/java/org/jongo/config/DatabaseConfiguration.java
+++ src/main/java/org/jongo/config/DatabaseConfiguration.java
@@ -31,38 +31,57 @@ public class DatabaseConfiguration {
     private static final Logger l = LoggerFactory.getLogger(DatabaseConfiguration.class);
     
     /**
-     * name of the database/schema. It should be unique for the whole jongo instance.
+     * Name of the configuration. It should be unique for the whole jongo instance. This is the name
+     * that will be used to access this configuration.
      */
     protected final String name;
     
     /**
-     * the {@link org.jongo.enums.JDBCDriver} driver
+     * the {@link org.jongo.enums.JDBCDriver} driver.
      */
     protected final JDBCDriver driver;
     
     /**
-     * the username used to authenticate against the RDMBS
+     * the username used to authenticate against the RDMBS.
      */
     protected final String username;
     
     /**
-     * the password for the given user used to authenticate against the RDMBS
+     * the password for the given user used to authenticate against the RDMBS.
      */
     protected final String password;
     
     /**
-     * the JDBC url of the RDMBS
+     * The name of the database to connect to.
      */
-    protected final String url;
+    protected final String database;
+    
+    /**
+     * Host of the RBDMS.
+     */
+    protected final String host;
+    
+    /**
+     * The port where the RDBMS is listening.
+     */
+    protected final Integer port;
+    
+    /**
+     * Set the connection to read-only.
+     */
+    protected final boolean readOnly;
 
     private boolean loaded = false;
 
-    public DatabaseConfiguration(String name, JDBCDriver driver, String username, String password, String url) {
+    private DatabaseConfiguration(String name, JDBCDriver driver, String username, String password, String database, String host, Integer port, boolean readOnly) {
         this.name = name;
         this.driver = driver;
         this.username = username;
         this.password = password;
-        this.url = url;
+        this.database = database;
+        this.host = host;
+        this.port = port;
+        this.readOnly = readOnly;
     }
     
     /**
@@ -74,8 +93,8 @@ public class DatabaseConfiguration {
      * @param url the JDBC url of the RDMBS
      * @return an instance of DatabaseConfiguration.
      */
-    public static DatabaseConfiguration instanceOf(String name, JDBCDriver driver, String user, String password, String url){
-        DatabaseConfiguration c = new DatabaseConfiguration(name, driver, user, password, url);
+    public static DatabaseConfiguration instanceOf(String name, JDBCDriver driver, String username, String password, String database, String host, Integer port, boolean readOnly){
+        DatabaseConfiguration c = new DatabaseConfiguration(name, driver, username, password, database, host, port, readOnly);
         c.loadDriver();
         return c;
     }
@@ -107,18 +126,59 @@ public class DatabaseConfiguration {
         return password;
     }
 
-    public String getUrl() {
-        return url;
-    }
-
     public String getUsername() {
         return username;
     }
-    
-    @Override
-    public String toString() {
-        return "DatabaseConfiguration{" + "name=" + name + ", driver=" + driver + ", user=" + username + ", password=" + password + ", url=" + url + '}';
+
+    public String getDatabase() {
+        return database;
+    }
+
+    public String getHost() {
+        return host;
+    }
+
+    public Integer getPort() {
+        return port;
+    }
+
+    public boolean isReadOnly() {
+        return readOnly;
     }
     
-    
+    public String toJdbcURL(){
+        final StringBuilder b = new StringBuilder("jdbc:");
+        switch(driver){
+            case HSQLDB_MEM:
+                b.append("hsqldb:mem:").append(database);
+                break;
+            case HSQLDB_FILE:
+                b.append("hsqldb:file:").append(database);
+                break;
+            case MSSQL:
+                b.append("microsoft:sqlserver://").append(host).append(":").append(port).append(";databasename=").append(database);
+                break;
+            case MSSQL_JTDS:
+                b.append("jtds:sqlserver://").append(host).append(":").append(port).append("/").append(database);
+                break;
+            case MySQL:
+                b.append("mysql://").append(host).append(":").append(port).append("/").append(database);
+                break;
+            case ORACLE:
+                b.append("oracle:thin:@//").append(host).append(":").append(port).append("/").append(database);
+                break;
+            case PostgreSQL:
+                b.append("postgresql://").append(host).append(":").append(port).append("/").append(database);
+                break;
+        }
+        return b.toString();
+    }
+
+    @Override
+    public String toString() {
+        return "DatabaseConfiguration{" + "name=" + name + ", driver=" 
+                + driver + ", username=" + username + ", password=" 
+                + password + ", database=" + database + ", host=" 
+                + host + ", port=" + port + ", readOnly=" + readOnly + '}';
+    }
 }
